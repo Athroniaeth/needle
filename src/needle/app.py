b1f09d34a16407aa19c24f28389c774afe2a8b7b
Athroniaeth <pierre.chaumont@hotmail.fr>
2024-11-24 10:19:38 +0100

    Update, add docker compose configuration

--- src/needle/app.py
+++ src/needle/app.py
@@ -1,12 +1,27 @@
 import gradio as gr
 from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
 
 from needle.interface import blocks
 
 app = FastAPI()
 
+
+@app.get(path="/hello")
+def hello():
+    return {"hello": "world"}
+
+
+"""
+Don't use '/' for path, gradio prevents prometheus 
+from retrieving information as gradio modifies routes.
+Routes defined as /metrics are hidden by the Gradio application mounted on '/'.
+"""
 app = gr.mount_gradio_app(
     app=app,
-    path="/",
+    path="/app",
     blocks=blocks,
 )
+
+# Initialize and configure the Prometheus instrument
+Instrumentator().instrument(app).expose(app)
