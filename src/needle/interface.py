b929de3a597045fc2c64287b1e12eb19d21edeed
Athroniaeth <pierre.chaumont@hotmail.fr>
2024-12-01 23:58:13 +0100

    Update, simplify skeleton interface

--- src/needle/interface.py
+++ src/needle/interface.py
@@ -1,30 +1,43 @@
-import gradio as gr
-import random
 import time
+from dataclasses import dataclass
+from typing import List
+
+import gradio as gr
+
+
+@dataclass
+class Message:
+    role: str
+    title: str
+    content: str
+
+    @classmethod
+    def from_gradio_dict(cls, data: dict):
+        return cls(role=data["role"], title=data['metadata']['title'], content=data['content'])
+
+
+def yes(message: str, history: List[dict]):
+    history = [Message.from_gradio_dict(x) for x in history]
+    print(type(message), type(history))
+    print(history)
+    time.sleep(5)
+    return "yes"
+
+
+def vote(data: gr.LikeData):
+    if data.liked:
+        print("You upvoted this response: ", data.value)
+    else:
+        print("You downvoted this response: ", data.value)
+
 
 with gr.Blocks() as demo:
-    with gr.Column(variant="compact"):
-        chatbot = gr.Chatbot(type="messages")
-
-        with gr.Row():
-            msg = gr.Textbox()
-            clear = gr.Button("Clear")
-
-    def user(user_message, history: list):
-        return "", history + [{"role": "user", "content": user_message}]
-
-    def bot(history: list):
-        bot_message = random.choice(["How are you?", "I love you", "I'm very hungry"])
-        history.append({"role": "assistant", "content": ""})
-        for character in bot_message:
-            history[-1]['content'] += character
-            time.sleep(0.01)
-            yield history
-
-    msg.submit(user, [msg, chatbot], [msg, chatbot], queue=False).then(
-        bot, chatbot, chatbot
+    chatbot = gr.Chatbot(
+        placeholder="<h1 style='text-align: center;'>Chatbot</h1><strong>Enter any ask or say something</strong>",
+        type="messages"
     )
-    clear.click(lambda: None, None, chatbot, queue=False)
 
-if __name__ == "__main__":
-    demo.launch()
+    chatbot.like(vote)
+    gr.ChatInterface(fn=yes, type="messages", chatbot=chatbot)
+
+demo.launch()
